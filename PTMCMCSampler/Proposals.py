ee1bc1c00162208b5e97f4a8a15424797cd2c10b
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-07-17 11:11:49 +0100

    adding MultiComponentAdaptiveGaussian

--- PTMCMCSampler/Proposals.py
+++ PTMCMCSampler/Proposals.py
@@ -69,7 +69,7 @@ class JumpProposal(object):
                         self.name, " and ".join(keys),
                         " and ".join(kwargs.keys())))
 
-    def assign_kwargs(self, kwargs):
+    def assign_kwargs(self, keys, kwargs):
         """Assign the kwargs to the class
 
         Parameters
@@ -77,7 +77,7 @@ class JumpProposal(object):
         kwargs: dict
             dictionary of kwargs
         """
-        for i in kwargs.keys():
+        for i in keys:
             setattr(self, i, kwargs[i])
 
     def return_new_samples(self, samples):
@@ -101,7 +101,7 @@ class SingleComponentAdaptiveCovariance(JumpProposal):
         self.name = "SingleComponentAdaptiveCovariance"
         required_kwargs = ["groups", "beta", "U", "S"]
         self.check_kwargs(kwargs, required_kwargs)
-        self.assign_kwargs(kwargs)
+        self.assign_kwargs(required_kwargs, kwargs)
 
     def __call__(self, samples):
         return super(SingleComponentAdaptiveCovariance, self).__call__(
@@ -171,11 +171,43 @@ class MultiComponentAdaptiveGaussian(JumpProposal):
     def __init__(self):
         super(MultiComponentAdaptiveGaussian, self).__init__()
         self.name = "MultiComponentAdaptiveGaussian"
+        required_kwargs = ["naccepted", "iter", "chain"]
+        self.check_kwargs(kwargs, required_kwargs)
+        self.assign_kwargs(required_kwargs, kwargs)
 
     def __call__(self):
+        return super(MultiComponentAdpativeGaussian, self).__call__(
+            self.jump, self.samples)
+
+    def jump(self, samples):
+        """Return the new samples assuming a Differential Evolution jump
+        proposal
+
+        Parameters
+        ----------
+        samples: list
+            list of samples
         """
-        """
-        return super(MultiComponentAdpativeGaussian, self).__call__()
+        new_samples = samples.copy()
+        n_jump_ind = np.random.randint(0, len(new_samples))
+        jumpind = np.random.randint(0, len(new_samples), n_jump_ind)
+        acc_rate = self.naccepted / self.iter
+
+        scaling_factor = 1.0 / 100
+        for ind in jumpind:
+            if np.std(self.chain[:, ind]) == 0:
+                current_sigma = 1
+            else:
+                current_sigma = np.std(self.chain[:, ind])
+
+            scaled_samples = new_samples[ind] * scaling_factor * acc_rate
+            if acc_rate > 0.234:
+                sigma = current_sigma + scaled_samples
+            else:
+                sigma = current_sigma - scaled_samples
+            new_samples[ind] += np.random.normal(0, abs(sigma))
+        lqxy = 0
+        return q, lqxy
 
 
 class AdaptiveGaussian(JumpProposal):
@@ -183,12 +215,40 @@ class AdaptiveGaussian(JumpProposal):
     """
     def __init__(self):
         super(AdaptiveGaussian, self).__init__()
+        required_kwargs = ["naccepted", "iter", "chain"]
         self.name = "AdaptiveGaussian"
+        self.check_kwargs(kwargs, required_kwargs)
+        self.assign_kwargs(required_kwargs, kwargs)
 
     def __call__(self):
+        return super(AdaptiveGaussian, self).__call__(self.jump, self.samples)
+
+    def jump(self, samples):
+        """Return the new samples assuming a Differential Evolution jump
+        proposal
+
+        Parameters
+        ----------
+        samples: list
+            list of samples
         """
-        """
-        return super(AdaptiveGaussian, self).__call__()
+        new_samples = samples.copy()
+
+        acc_rate = self.naccepted / self.iter
+        scaling_factor = 1.0 / 100
+        for ind in range(len(new_samples)):
+            if np.std(self.chain[:, ind]) == 0:
+                current_sigma = 1
+            else:
+                current_sigma = np.std(self.chain[:, ind])
+
+            scaled_samples = new_samples[ind] * scaling_factor * acc_rate
+            if acc_rate > 0.234:
+                sigma = current_sigma + scaled_samples
+            else:
+                sigma = current_sigma - scaled_samples
+            new_samples += np.random.normal(0, sigma)
+        return new_samples
 
 
 class DifferentialEvolution(JumpProposal):
@@ -206,7 +266,7 @@ class DifferentialEvolution(JumpProposal):
         required_kwargs = ["beta", "groups", "DEBuffer"]
         self.name = "DifferentialEvolution"
         self.check_kwargs(kwargs, required_kwargs)
-        self.assign_kwargs(kwargs)
+        self.assign_kwargs(required_kwargs, kwargs)
 
     def __call__(self, samples):
         return super(DifferentialEvolution, self).__call__(self.jump, self.samples)
@@ -260,7 +320,7 @@ class Normal(JumpProposal):
         required_kwargs = ["step_size"]
         self.name = "Normal"
         self.check_kwargs(kwargs, required_kwargs)
-        self.assign_kwargs(kwargs)
+        self.assign_kwargs(required_kwargs, kwargs)
 
     def __call__(self, samples):
         return super(Normal, self).__call__(self.jump, samples)
@@ -291,7 +351,7 @@ class Uniform(JumpProposal):
         required_kwargs = ["pmin", "pmax"]
         self.name = "Uniform"
         self.check_kwargs(kwargs, required_kwargs)
-        self.assign_kwargs(kwargs)
+        self.assign_kwargs(required_kwargs, kwargs)
 
     def __call__(self, samples):
         return super(Uniform, self).__call__(self.jump, samples)
