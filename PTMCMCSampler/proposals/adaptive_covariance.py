dbde9db8522dfbdd624433ac36749f68244759b1
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-07-17 11:54:20 +0100

    adding AdaptiveCovariance

--- PTMCMCSampler/proposals/adaptive_covariance.py
+++ PTMCMCSampler/proposals/adaptive_covariance.py
@@ -61,3 +61,42 @@ class AdaptiveCovariance(JumpProposal):
     def __init__(self):
         super(AdaptiveCovariance, self).__init__()
         self.name = "AdaptiveCovariance"
+
+    def __call__(self, samples):
+        super(AdaptiveCovariance, self).__call__(self.jump, samples)
+
+    def jump(self, samples):
+        """Return the new samples assuming a Differential Evolution jump
+        proposal
+
+        Parameters
+        ----------
+        samples: list
+            list of samples
+        """
+        new_samples = samples.copy()
+
+        jumpind = np.random.randint(0, len(self.groups))
+        ndim = len(self.groups[jumpind])
+
+        prob = np.random.rand()
+        if prob > 0.97:
+            scale = 10
+        elif prob > 0.9:
+            scale = 0.2
+        else:
+            scale = 1.0
+
+        if 1.0 / self.beta <= 100:
+            scale *= np.sqrt(1.0 / self.beta)
+
+        y = np.dot(self.U[jumpind].T, x[self.groups[jumpind]])
+
+        ind = np.arange(len(self.groups[jumpind]))
+        neff = len(ind)
+        cd = 2.4 / np.sqrt(2 * neff) * scale
+
+        y[ind] += np.random.randn(neff) * cd * np.sqrt(self.S[jumpind][ind])
+        new_samples[self.groups[jumpind]] = np.dot(self.U[jumpind], y)
+
+        return new_samples
