314fd48f27dbcfc389e0771be12f0997e4605171
rhys.green <rhys.green@ligo.org>
2019-07-19 10:46:31 +0100

    making write to file an optional argument

--- PTMCMCSampler/PTMCMCSampler.py
+++ PTMCMCSampler/PTMCMCSampler.py
@@ -158,7 +158,9 @@ class PTSampler(object):
         thin=10,
         i0=0,
         neff=100000,
+        write_cold_chains=False,
         writeHotChains=False,
+        save_jump_stats = False,
         hotChain=False,
     ):
         """
@@ -189,6 +191,8 @@ class PTSampler(object):
         self.Niter = Niter
         self.neff = neff
         self.tstart = 0
+        self.write_cold_chains= write_cold_chains
+        self.save_jump_stats = save_jump_stats
 
         N = int(maxIter / thin)
 
@@ -294,9 +298,10 @@ class PTSampler(object):
                 self.resumechain = np.loadtxt(self.fname)
                 self.resumeLength = self.resumechain.shape[0]
             self._chainfile = open(self.fname, "a")
-        else:
+            self._chainfile.close()
+        elif self.write_cold_chains:
             self._chainfile = open(self.fname, "w")
-        self._chainfile.close()
+            self._chainfile.close()
 
     def updateChains(self, p0, lnlike0, lnprob0, iter):
         """
@@ -315,23 +320,24 @@ class PTSampler(object):
             self._lnprob[ind] = lnprob0
 
         # write to file
-        if iter % self.isave == 0 and iter > 1 and iter > self.resumeLength:
-            if self.writeHotChains or self.MPIrank == 0:
-                self._writeToFile(iter)
+        if self.write_cold_chains :
+            if iter % self.isave == 0 and iter > 1 and iter > self.resumeLength:
+                if self.writeHotChains or self.MPIrank == 0:
+                    self._writeToFile(iter)
 
-            # write output covariance matrix
-            np.save(self.outDir + "/cov.npy", self.cov)
-            if self.MPIrank == 0 and self.verbose and iter > 1:
-                sys.stdout.write("\r")
-                sys.stdout.write(
-                    "Finished %2.2f percent in %f s Acceptance rate = %g"
-                    % (
-                        iter / self.Niter * 100,
-                        time.time() - self.tstart,
-                        self.naccepted / iter,
+                # write output covariance matrix
+                np.save(self.outDir + "/cov.npy", self.cov)
+                if self.MPIrank == 0 and self.verbose and iter > 1:
+                    sys.stdout.write("\r")
+                    sys.stdout.write(
+                        "Finished %2.2f percent in %f s Acceptance rate = %g"
+                        % (
+                            iter / self.Niter * 100,
+                            time.time() - self.tstart,
+                            self.naccepted / iter,
+                        )
                     )
-                )
-                sys.stdout.flush()
+                    sys.stdout.flush()
 
     def sample(
         self,
@@ -359,7 +365,9 @@ class PTSampler(object):
         thin=10,
         i0=0,
         neff=100000,
+        write_cold_chains= False,
         writeHotChains=False,
+        save_jump_stats = False,
         hotChain=False,
     ):
         """
@@ -426,6 +434,7 @@ class PTSampler(object):
                 i0=i0,
                 neff=neff,
                 writeHotChains=writeHotChains,
+                write_cold_chains= write_cold_chains,
                 hotChain=hotChain,
             )
 
@@ -804,6 +813,7 @@ class PTSampler(object):
             )
         self._chainfile.close()
 
+
         #### write jump statistics files ####
 
         # only for T=1 chain
@@ -906,8 +916,9 @@ class PTSampler(object):
         # add to jump dictionary and initialize file
         if func.__name__ not in self.jumpDict:
             self.jumpDict[func.__name__] = [0, 0]
-            fout = open(self.outDir + "/" + func.__name__ + "_jump.txt", "w")
-            fout.close()
+            if self.save_jump_stats:
+                fout = open(self.outDir + "/" + func.__name__ + "_jump.txt", "w")
+                fout.close()
 
     # add auxilary jump proposal distribution functions
     def addAuxilaryJump(self, func):
