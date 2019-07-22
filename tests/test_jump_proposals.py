6a25408c3dfd9b09310a97339c630be6f6e910b8
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-07-22 10:54:10 +0100

    adding more tests

--- tests/test_jump_proposals.py
+++ tests/test_jump_proposals.py
@@ -24,7 +24,7 @@ class Base(object):
         new_samples, prob = self.class_object(self.samples, kwargs=None)
         assert len(new_samples) == len(self.samples)
         for i in new_samples:
-            assert isinstance(i, np.float64)
+            assert isinstance(i, float)
 
     def test_1d_case(self):
         """Test the __call__ method for ndim = 1
@@ -33,7 +33,30 @@ class Base(object):
         new_samples, prob = self.class_object(self.samples, kwargs=None)
         assert len(new_samples) == len(self.samples)
         for i in new_samples:
-            assert isinstance(i, np.float64)
+            assert isinstance(i, float)
+
+
+class TestNormal(Base):
+    """Test the Normal jump proposal
+    """
+    def setup(self):
+        """Setup the Uniform class
+        """
+        kwargs = {"step_size": 0.1}
+        self.class_object = super(TestNormal, self).setup("Normal", kwargs)
+
+    def test_no_kwargs(self):
+        """Test to make sure that an Exception is raised when you fail to pass
+        the kwargs to initalize the class
+        """
+        with pytest.raises(Exception) as info:
+            class_object = super(TestNormal, self).setup("Normal", {})
+
+    def test_call(self):
+        """Test the __call__ method for the Uniform class
+        """
+        super(TestNormal, self).test_1d_case()
+        super(TestNormal, self).test_2d_case()
 
 
 class TestUniform(Base):
@@ -44,6 +67,13 @@ class TestUniform(Base):
         """
         kwargs = {"pmin": 0.0, "pmax": 10.0}
         self.class_object = super(TestUniform, self).setup("Uniform", kwargs)
+
+    def test_no_kwargs(self):
+        """Test to make sure that an Exception is raised when you fail to pass
+        the kwargs to initalize the class
+        """
+        with pytest.raises(Exception) as info:
+            class_object = super(TestNormal, self).setup("Normal", {})
 
     def test_call(self):
         """Test the __call__ method for the Uniform class
