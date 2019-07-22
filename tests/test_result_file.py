25d8cf23b8a0036fabbe5abfdc06699eee0ac1fd
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-07-22 16:41:20 +0100

    adding result class

---
+++ tests/test_result_file.py
@@ -0,0 +1,11 @@
+import pytest
+from PTMCMCSampler.result import Result
+
+
+class TestResult(object):
+    """Class to test the Result object
+    """
+    def setup(self):
+        """Setup the Result object
+        """
+        
