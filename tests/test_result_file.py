6723870ef67860cb2be2e0565669dd756b6b212e
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-07-22 17:24:03 +0100

    adding result

--- tests/test_result_file.py
+++ tests/test_result_file.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from PTMCMCSampler.result import Result
 
 
@@ -8,4 +9,23 @@ class TestResult(object):
     def setup(self):
         """Setup the Result object
         """
+        self.initial_samples = np.array(
+            [np.random.random(2) for i in range(1000)])
+        self.burnin = 100
+        self.result = Result(self.initial_samples, burnin=self.burnin)
+
+    def test_set_burnin(self):
+        """Test the function `set_burnin`
+        """
+        assert self.result.burnin == self.burnin
+        self.result.set_burnin(500)
+        assert self.result.burnin == 500
+        self.result.set_burnin(self.burnin)
+        assert self.result.burnin == self.burnin
+
+    def test_samples(self):
+        """Test that the samples property returns what it should
+        """
+        self.result.samples.all() == self.initial_samples[self.burnin:].all()
+
         
