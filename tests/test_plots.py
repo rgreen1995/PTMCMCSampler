ab25992de939df4150350b5c01ba2afd05ab8319
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-07-22 19:32:06 +0100

    adding plotting tests

---
+++ tests/test_plots.py
@@ -0,0 +1,20 @@
+import pytest
+import matplotlib
+import numpy as np
+from PTMCMCSampler import plots
+
+
+def test_chain_plots():
+    """Test that we can produce a chain plot
+    """
+    samples = np.array([np.random.random(2) for i in range(500)])
+    assert isinstance(plots.chains_plot(samples), matplotlib.figure.Figure)
+
+
+def test_corner_plots():
+    """Test that we can produce a corner plot
+    """
+    samples = np.array([np.random.random(2) for i in range(500)])
+    assert isinstance(plots.corner_plot(samples), matplotlib.figure.Figure)
+    assert isinstance(
+        plots.corner_plot(samples, truths=[0.5, 0.1]), matplotlib.figure.Figure)
